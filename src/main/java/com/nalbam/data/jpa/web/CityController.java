d0739b941fa5d88cb56ed919644d6b0eac777313
nalbam-ya <jyyu@yanolja.in>
2016-07-15 00:20:35 +0900

    init

---
+++ src/main/java/com/nalbam/data/jpa/web/CityController.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2012-2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.nalbam.data.jpa.web;
+
+import com.nalbam.data.jpa.service.CityService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/city")
+public class CityController {
+
+    @Autowired
+    private CityService cityService;
+
+    @ResponseBody
+    @Transactional(readOnly = true)
+    @RequestMapping(method = RequestMethod.GET, value = "")
+    public String get(@RequestParam(value = "state", defaultValue = "Bath") String state,
+                      @RequestParam(value = "country", defaultValue = "UK") String country) {
+        return this.cityService.getCity(state, country).toString();
+    }
+
+}
