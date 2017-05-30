2f687ceaec4908d64bb54969812cb1ba04f97e54
nalbam-ya <jyyu@yanolja.in>
2017-05-30 09:57:51 +0900

    nalbam-common

--- src/main/java/com/nalbam/config/client/controller/HealthController.java
+++ src/main/java/com/nalbam/config/client/controller/HealthController.java
@@ -1,12 +1,11 @@
 package com.nalbam.config.client.controller;
 
+import com.nalbam.common.util.AddressUtil;
+import com.nalbam.common.util.VersionUtil;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.net.InetAddress;
-import java.net.NetworkInterface;
-import java.util.Enumeration;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -17,43 +16,9 @@ public class HealthController {
     @RequestMapping(method = RequestMethod.GET, value = "")
     public Map<String, String> index() {
         Map<String, String> data = new ConcurrentHashMap<>();
-        data.put("version", getVersion());
-        data.put("address", getAddress());
+        data.put("address", AddressUtil.getAddress());
+        data.put("version", VersionUtil.getVersion(getClass()));
         return data;
-    }
-
-    private synchronized String getAddress() {
-        try {
-            InetAddress addr;
-            for (Enumeration<NetworkInterface> e = NetworkInterface.getNetworkInterfaces(); e.hasMoreElements(); ) {
-                for (Enumeration<InetAddress> a = e.nextElement().getInetAddresses(); a.hasMoreElements(); ) {
-                    addr = a.nextElement();
-                    if (!addr.isLoopbackAddress() && !addr.isLinkLocalAddress() && addr.isSiteLocalAddress()) {
-                        return addr.getHostAddress();
-                    }
-                }
-            }
-            addr = InetAddress.getLocalHost();
-            return addr.getHostAddress();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return "";
-    }
-
-    private synchronized String getVersion() {
-        String version = null;
-        Package p = getClass().getPackage();
-        if (p != null) {
-            version = p.getImplementationVersion();
-            if (version == null) {
-                version = p.getSpecificationVersion();
-            }
-        }
-        if (version == null) {
-            version = "";
-        }
-        return version;
     }
 
 }
