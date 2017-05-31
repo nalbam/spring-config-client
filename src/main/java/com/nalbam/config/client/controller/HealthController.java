cb50f5a75c5e1691bee3183d1d62d12c75033cb9
nalbam-ya <jyyu@yanolja.in>
2017-05-31 16:33:56 +0900

    nalbam-common-2.1.0

--- src/main/java/com/nalbam/config/client/controller/HealthController.java
+++ src/main/java/com/nalbam/config/client/controller/HealthController.java
@@ -1,7 +1,7 @@
 package com.nalbam.config.client.controller;
 
 import com.nalbam.common.util.AddressUtil;
-import com.nalbam.common.util.VersionUtil;
+import com.nalbam.common.util.PackageUtil;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,8 +16,9 @@ public class HealthController {
     @RequestMapping(method = RequestMethod.GET, value = "")
     public Map<String, String> index() {
         Map<String, String> data = new ConcurrentHashMap<>();
+        data.put("title", PackageUtil.getTitle(getClass()));
+        data.put("version", PackageUtil.getVersion(getClass()));
         data.put("address", AddressUtil.getAddress());
-        data.put("version", VersionUtil.getVersion(getClass()));
         return data;
     }
 
