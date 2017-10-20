3ea1f8857af236bc108673f5eb9441b485f60759
nalbam-ya <jyyu@yanolja.com>
2017-10-20 18:23:51 +0900

    c

--- src/main/java/com/nalbam/config/client/ConfigClientApplication.java
+++ src/main/java/com/nalbam/config/client/ConfigClientApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@Slf4j
 @EnableScheduling
 @SpringBootApplication
 public class ConfigClientApplication {
