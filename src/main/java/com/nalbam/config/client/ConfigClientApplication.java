3a07e0c17bf3f17a2831d0daa1398daff069c057
nalbam <me@nalbam.com>
2018-04-23 15:22:00 +0900

    eureka-client

--- src/main/java/com/nalbam/config/client/ConfigClientApplication.java
+++ src/main/java/com/nalbam/config/client/ConfigClientApplication.java
@@ -2,9 +2,9 @@ package com.nalbam.config.client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableScheduling
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ConfigClientApplication {
 
--- src/main/java/com/nalbam/config/client/controller/HomeController.java
+++ src/main/java/com/nalbam/config/client/controller/HomeController.java
@@ -1,16 +1,30 @@
 package com.nalbam.config.client.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @Slf4j
 @RestController
 public class HomeController {
 
-    @GetMapping("/health")
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    @GetMapping("/")
     public String health() {
         return "OK";
+    }
+
+    @GetMapping("/service-instances/{applicationName}")
+    public List<ServiceInstance> serviceInstancesByApplicationName(@PathVariable String applicationName) {
+        return this.discoveryClient.getInstances(applicationName);
     }
 
 }
