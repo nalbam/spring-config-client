48086f974dcb3fce1417908422f5254ce777b7e9
nalbam <me@nalbam.com>
2018-04-23 23:45:11 +0900

    error_rate

--- src/main/java/com/nalbam/config/client/controller/HelloController.java
+++ src/main/java/com/nalbam/config/client/controller/HelloController.java
@@ -2,6 +2,7 @@ package com.nalbam.config.client.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
@@ -13,14 +14,19 @@ import java.util.Random;
 public class HelloController {
 
     @GetMapping(value = "/hello")
-    public String hello() {
-        log.info("Access /hello");
+    public String hello(@RequestParam(value = "rate", defaultValue = "100") Integer rate) {
+        log.info("Access /hello?rate=" + rate);
 
         List<String> greetings = Arrays.asList("Hi there", "Greetings", "Lok'tar ogar");
-        Random rand = new Random();
 
-        int randomNum = rand.nextInt(greetings.size());
-        return greetings.get(randomNum);
+        Random random = new Random();
+
+        if (rate > random.nextInt(100)) {
+            return greetings.get(random.nextInt(greetings.size()));
+        }
+
+        // Error
+        return greetings.get(greetings.size());
     }
 
 }
