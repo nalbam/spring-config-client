8adcf9899e2699cf0d702a696e12379e67c3af54
nalbam <me@nalbam.com>
2018-04-26 01:45:15 +0900

    task_true

--- src/main/java/com/nalbam/config/client/task/HelloTask.java
+++ src/main/java/com/nalbam/config/client/task/HelloTask.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class HelloTask {
 
-    private Boolean enabled = false;
+    private Boolean enabled = true;
 
     @Autowired
     private HelloRequestService helloRequestService;
